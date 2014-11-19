fa46f14b79c35d4930078a510789317f07a085d5
oscar <oscar.escaloni@tuguu.com>
2014-11-19 14:12:10 +0000

    added stateProvider feature

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,25 +9,59 @@
  * Main module of the application.
  */
 angular
-  .module('angularAndPluginsAppApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('angularAndPluginsAppApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ui.router'
+    ])
+
+    /*.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })*/
+
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: "/",
+                templateUrl: "views/main.html",
+                controller: 'MainCtrl',
+            })
+      
+            .state('about', {
+                url: "/about",
+                templateUrl: "views/about.html",
+                controller: 'AboutCtrl',
+            })
+        ;
+
+        $urlRouterProvider.otherwise('/');
+        
+        /*$routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });*/
+    }])
+;
